b44684a7d81c41bbd25ae38c40f525e47d268dee
Ajay Kumar S <soleil.fils.88@gmail.com>
2019-07-29 23:11:37 +0530

    fix unused code vulnerabilities

--- src/main/java/com/techprimers/serverless/SpringCloudFunctionExampleApplication.java
+++ src/main/java/com/techprimers/serverless/SpringCloudFunctionExampleApplication.java
@@ -2,11 +2,6 @@ package com.techprimers.serverless;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SpringCloudFunctionExampleApplication {
